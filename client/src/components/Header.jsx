97bce8ea046bb3858d0861b60b0c2959a9abf26c
Vlad <vladsa000@gmail.com>
2023-02-03 22:28:33 +0200

    added register page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
             d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
           />
         </svg>
-        <span className="font-bold text-xl">airbnc</span>
+        <span className="font-bold text-xl">airbnb</span>
       </Link>
       <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300">
         <div>Anywhere</div>
